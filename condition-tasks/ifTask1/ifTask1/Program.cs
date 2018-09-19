96f93932525b769e08725d9d3576a937c4e5aae6
matpyy <matias.pyyhtia09@gmail.com>
2018-09-19 09:10:15 +0300

    condition and loop tasks  folders defined

---
+++ condition-tasks/ifTask1/ifTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ifTask1
+{
+    class Program
+
+        //oneline comment
+        /*multiline 
+         * comment */
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko annettu luku " + 
+                "positiivinen, " +
+                "negatiivinen vai nolla.");
+
+            Console.WriteLine ("")
+        }
+    }
+}
