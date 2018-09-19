baac67a52b8d66ca13541a41249212dcde54d699
matpyy <matias.pyyhtia09@gmail.com>
2018-09-19 09:57:13 +0300

    added task number 1

--- condition-tasks/ifTask1/ifTask1/Program.cs
+++ condition-tasks/ifTask1/ifTask1/Program.cs
@@ -5,8 +5,16 @@ namespace ifTask1
     class Program
 
         //oneline comment
+
         /*multiline 
-         * comment */
+         * int
+         * String
+         * double
+         * bool
+         * char
+         * 
+         * comment     
+         */
     {
         static void Main(string[] args)
         {
@@ -14,7 +22,27 @@ namespace ifTask1
                 "positiivinen, " +
                 "negatiivinen vai nolla.");
 
-            Console.WriteLine ("")
+            Console.Write("Syötä luku:");
+            String userInput = Console.ReadLine();
+
+            Console.WriteLine($"Syötit luvun: {userInput}");
+                int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"Numero {number} on nolla!");  //tulostaa käyttäjälle että numero on 0
+            else if (number<0)
+                Console.WriteLine($"Numero {number} on negatiivinen!"); //tulostaa käyttäjälle että numero on negatiivinen
+            else 
+                Console.WriteLine($"Numero {number} on positiivinen!"); //tulostaa käyttäjälle että numero on positiivinen
+
+
+
+
+
+
+
+
+
         }
     }
 }
