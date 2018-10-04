5a9dede297d07c33b31231b45e1e0ff417dc003d
matpyy <matias.pyyhtia09@gmail.com>
2018-10-04 10:15:39 +0300

    added loopTask3_3 folder

---
+++ loop-tasks/loopTask3_3/loopTask3_3/loopTask3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loopTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
