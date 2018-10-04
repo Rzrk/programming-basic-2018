4bcc2e68e8053cfdb90507684765e51acf71b5a6
matpyy <matias.pyyhtia09@gmail.com>
2018-10-04 09:30:51 +0300

    added loopTask3_1

--- loop-tasks/loopTask3_1/loopTask3_1/loopTask3_1/Program.cs
+++ loop-tasks/loopTask3_1/loopTask3_1/loopTask3_1/Program.cs
@@ -6,7 +6,42 @@ namespace loopTask3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskeee annetun luvun kertoman");
+            
+            int number = 0;
+            int i = 1;
+            int fact = 1;
+            do
+            {
+                Console.Write("syötä luku:");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("virheellinen syöte!");
+                }
+            } while (number <= 0);
+                                                                                         //calculate factorial 1 * 2 * 3 * ... * n
+                while (i <= number)
+                {
+                fact = fact * i;    //fact*=i
+                i = i + 1;         //i++;
+
+                }
+
+                Console.WriteLine($"Luvun {number} ! = {fact}");
+
+            /*
+             * for (i = 1; i <=; i++)
+             * {
+             *      fact * = i;
+             * }
+             * */
+     
+      
+
         }
     }
 }
