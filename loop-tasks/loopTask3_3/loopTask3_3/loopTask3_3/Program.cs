3a8b46993d38c311dc06e450b289719f48edafb8
matpyy <matias.pyyhtia09@gmail.com>
2018-10-04 10:40:47 +0300

    Updated loopTask3_3

--- loop-tasks/loopTask3_3/loopTask3_3/loopTask3_3/Program.cs
+++ loop-tasks/loopTask3_3/loopTask3_3/loopTask3_3/Program.cs
@@ -6,7 +6,35 @@ namespace loopTask3_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+           
+            Console.WriteLine("Ohjelma laskee parittomien ja parillisten lukujen summan!");
+            Console.Write("syötä lukusi:");
+            number = int.Parse(Console.ReadLine());
+            
+            while(i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i; //evenSum += i;
+                    
+                }
+                
+                else
+                {
+                    oddSum = oddSum + i; //oddSum += i;
+                    
+
+                }
+                i++;
+            }
+            Console.WriteLine($"parillisten summa on {evenSum}");
+            Console.WriteLine($"parittomien summa on {oddSum}");
+
+
         }
     }
 }
