49fe930175aa3e5c28adc512fbc413bde6905bbe
matpyy <matias.pyyhtia09@gmail.com>
2018-10-28 21:40:20 +0200

    added some function tasks

---
+++ function-tasks/functionTask1_1/functionTask1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace functionTask1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä ohjelmalle sana tai lause ohjelma tulostaa lauseesi.");
+
+
+            System.out.print("Anna viesti: ");
+            String viesti = input.nextLine();
+            System.out.print("Anna toistokerrat: ");
+            int kerrat = Integer.parseInt(input.nextLine());
+
+            for (int i = 0; i < kerrat; i++)
+            {
+                System.out.println(viesti);
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
---
+++ function-tasks/functionTask1_2/functionTask1_2/functionTask1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace functionTask1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;   
+            Intro(out x, out y);
+            Console.WriteLine(SmallerNumber(x, y));
+        }
+        static void Intro(out int number1, out int number2)
+        { 
+            Console.WriteLine("Syötä ohjelmalle luku, funktio palauttaa arvonaan kahdesta annetusta luvusta pienemmän");
+            Console.Write("syötä luku1:");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("syötä luku2:");
+            number2 = int.Parse(Console.ReadLine());
+        }
+
+        static string SmallerNumber(int a, int b)
+        {
+            if (a == b)
+                return $"Luku {a} on yhtäsuuri kuin {b}";
+            else if (a < b)
+                return $"luku {b} on isompi kuin {a}";
+            else
+                return $"luku {b} on pienempi kuin {a}";
+
+
+        }
+
+            
+            
+        }
+    }
