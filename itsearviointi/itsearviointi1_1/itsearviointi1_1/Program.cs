3c0b5b7f79f46e84e7a8cf18287bdf38d606ede3
matpyy <matias.pyyhtia09@gmail.com>
2018-10-29 11:19:50 +0200

    Added itsearviointi folder

---
+++ itsearviointi/itsearviointi1_1/itsearviointi1_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace itsearviointi1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma toistaa käyttäjän kirjoittaman lauseen viisinkertaisesti");
+            Console.WriteLine("Kirjoita ohjelmalle lause:");
+
+            int number = 0;
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine("Kirjoita ohjelmalle lause:");
+                string userInput = Console.ReadLine();
+                number = int.TryParse(userInput);
+
+                if (number <= 5)
+                {
+
+
+
+                }
+
+
+            }
+
+
+        }
+    }
+}
