17fa376dff8ecf795d9dfe151c948e5dfd6a6b89
matpyy <matias.pyyhtia09@gmail.com>
2018-10-29 11:09:06 +0200

    added functionTask1_3

---
+++ function-tasks/functionTask1_3/functionTask1_3/functionTask1_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace functionTask1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int retNumber = NumberFromRange(-10, 10);
+            Console.Write($"syötit sallitun luvun {retNumber}");
+        }
+
+        static int NumberFromRange(int lowerBound, int upperBound)
+        {
+
+            do
+            {
+                Console.WriteLine($"Syötä luku alueelta {lowerBound} ... {upperBound} ");
+                string userInput = Console.ReadLine();
+                int userNumber = int.Parse(userInput);
+
+
+                if (userNumber >= lowerBound && userNumber <= upperBound)
+
+                return userNumber;
+
+            
+        
+                else
+                
+                    Console.WriteLine("väärä syöte!");
+
+
+                } while (true);
+
+
+        }
+    }
+
+}
