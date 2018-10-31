12c566425cb62b73b0b0f91b75f7cec6653271f5
matpyy <matias.pyyhtia09@gmail.com>
2018-10-31 13:31:22 +0200

    Added arrayTasks folder and arrayTask1_1 example

---
+++ array-tasks/arrayTask1_1/arrayTask1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace arrayTask1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma alustaa kokonaisluku taulukon(10 alkiota) johon arvotaan lukuja 0....100");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+            //set value to array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+            // get values from array numbers
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+            Console.WriteLine("ohjelma päättyy");
+
+        }
+
+    }
+}
---
+++ array-tasks/arrayTask1_1_2/arrayTask1_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrayTask1_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // tällä käskyllä saadaan UTF8 merkit
+            int[] productAmount = new int[] { 1, 2, 3 };
+            Decimal[] productPrice = new Decimal[3] { 7.96m, 72.0m, 99.90m };
+            Decimal[] productTotalPrice = new Decimal[3];
+            Console.WriteLine("Hinnasto");
+
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"tuote 1 + {i + 1}.{productTotalPrice[i]:c}");
+            }
+        }
+    }
+}
