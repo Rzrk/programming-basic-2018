2223888e26a0071ec8f9ea29e76fa7c06c73e4ad
matpyy <matias.pyyhtia09@gmail.com>
2019-01-15 17:49:09 +0200

    recovered

---
+++ filehandling-tasks/fileHandling1_1/ExampleFileClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace ExampleFileClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Read in every line in specified file.
+            // ... This will store all lines in an array in memory.
+            string[] lines = File.ReadAllLines(@"C:\TEMP\TESTI.DAT");
+            foreach (string line in lines)
+            {
+                // Do something with the line.
+                if (line.Length > 80)
+                {
+                    // Important code.
+                }
+            }
+        }
+
+    }
+}
